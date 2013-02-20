cbec2f1fddebc2f76820bc8bb30d80a98900b2af
TY@MacBook Pro <tianyu0915@gmail.com>
2013-02-20 11:50:58 +0800

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(name='caoliu',  
       author='TY',  
       author_email='tianyu0915@gmail.com',  
-      version='0.0.3',  
+      version='0.0.4',  
       description='你懂',  
       keywords ='你懂',
       url='http://github.com/tianyu0915/caoliu',  
